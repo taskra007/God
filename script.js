896c5a901ac4f84c4a159291dea4e00fde9c7054
taskra007 <anbumass81@gmail.com>
2025-11-30 10:48:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,15 @@
-const questionContainer = document.querySelector(".question-container");
-const resultContainer = document.querySelector(".result-container.yes");
-const gifResult = document.querySelector(".gif-result");
-const heartLoader = document.querySelector(".cssload-main");
-const yesBtn = document.querySelector(".js-yes-btn");
-const noBtn = document.querySelector(".js-no-btn");
+const images = document.querySelectorAll(".gallery img");
+const modal = document.getElementById("modal");
+const modalImg = document.getElementById("modalImg");
+const closeBtn = document.getElementById("closeBtn");
 
-// /change the postion of no button
-noBtn.addEventListener("mouseover", () => {
-  const newX = Math.floor(Math.random() * questionContainer.offsetWidth);
-  const newY = Math.floor(Math.random() * questionContainer.offsetWidth);
-
-  noBtn.style.left = `${newX}px`;
-  noBtn.style.top = `${newY}px`;
+images.forEach(img => {
+    img.addEventListener("click", () => {
+        modal.style.display = "flex";
+        modalImg.src = img.src;
+    });
 });
 
-// yes button functionality
-
-yesBtn.addEventListener("click", () => {
-  questionContainer.style.display = "none";
-  heartLoader.style.display = "inherit";
-
-  const timeoutId = setTimeout(() => {
-    heartLoader.style.display = "none";
-    resultContainer.style.display = "inherit";
-    gifResult.play();
-  }, 3000);
+closeBtn.addEventListener("click", () => {
+    modal.style.display = "none";
 });
